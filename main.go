db732c0989e6841e6452bddbeca1bcd69a393f84
eu-micaeu <micaelrrocha13@gmail.com>
2023-04-01 22:22:32 -0300

    by Micael

--- main.go
+++ main.go
@@ -1,14 +1,39 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, "index.html")
-    })
-    fmt.Println("Servidor rodando na porta 8080...")
-    http.ListenAndServe(":8080", nil)
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./css"))))
+	http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("./img"))))
+	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("./js"))))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
+
+	http.HandleFunc("/jogodacobrinha", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "jogodacobrinha.html")
+	})
+
+	http.HandleFunc("/jogodaforca", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "jogodaforca.html")
+	})
+
+	http.HandleFunc("/jogodamemoria", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "jogodamemoria.html")
+	})
+
+	http.HandleFunc("/jogodavelha", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "jogodavelha.html")
+	})
+
+	http.HandleFunc("/jogodoclique", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "jogodoclique.html")
+	})
+
+	fmt.Println("Servidor rodando na porta 8080...")
+	http.ListenAndServe(":8080", nil)
 }
