2dcb598837a7dbd12e381829cb23f87988def715
eu-micaeu <micaelrrocha13@gmail.com>
2023-04-01 22:42:57 -0300

    by Micael

--- main.go
+++ main.go
@@ -6,31 +6,38 @@ import (
 )
 
 func main() {
-	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./css"))))
-	http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("./img"))))
-	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("./js"))))
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
+	http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("img"))))
+	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("js"))))
 
+    
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
 		http.ServeFile(w, r, "index.html")
 	})
 
-	http.HandleFunc("/jogodacobrinha", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/jogodacobrinha.html", func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
 		http.ServeFile(w, r, "jogodacobrinha.html")
 	})
 
-	http.HandleFunc("/jogodaforca", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/jogodaforca.html", func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
 		http.ServeFile(w, r, "jogodaforca.html")
 	})
 
-	http.HandleFunc("/jogodamemoria", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/jogodamemoria.html", func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
 		http.ServeFile(w, r, "jogodamemoria.html")
 	})
 
-	http.HandleFunc("/jogodavelha", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/jogodavelha.html", func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
 		http.ServeFile(w, r, "jogodavelha.html")
 	})
 
-	http.HandleFunc("/jogodoclique", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/jogodoclique.html", func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
 		http.ServeFile(w, r, "jogodoclique.html")
 	})
 
